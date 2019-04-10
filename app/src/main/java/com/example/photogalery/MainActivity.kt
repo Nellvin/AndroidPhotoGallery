7136ee981ae288a3ebdd9e79181b4d2b01853fb6
Filip <floffler@wp.pl>
2019-04-10 21:52:50 +0200

    Armia mew :)

--- app/src/main/java/com/example/photogalery/MainActivity.kt
+++ app/src/main/java/com/example/photogalery/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.photogalery
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var mData =ArrayList<MyData>(20)
+    val tags= ArrayList<String>()
+    val adapter = CustomAdapter(mData)
 
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +25,15 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById(R.id.recycleView) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
         initList()
-        val adapter = CustomAdapter(mData)
+
 
         recyclerView.adapter = adapter
+
+
     }
 
     private fun initList() {
-        val tags= ArrayList<String>()
+//        val tags= ArrayList<String>()
         tags.add("delo")
         tags.add("elo")
         tags.add("melo")
@@ -38,5 +45,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu,menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == R.id.mainMenu_addingbutton) {
+            mData.add(mData.size,MyData("ELO",tags, Date()))
+            adapter.notifyItemInserted(mData.size-1)
+            recycleView.scrollToPosition(mData.size-1)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
